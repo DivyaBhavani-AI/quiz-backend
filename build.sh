807f6a834938cf04a8032b467323c590547488b0
DivyaBhavani-AI <DivyaBhavani-AI@gmail.com>
2026-08-19 17:08:05 +0530

    Add superuser creation to build script

--- build.sh
+++ build.sh
@@ -3,3 +3,5 @@ set -o errexit
 pip install -r requirements.txt
 python manage.py collectstatic --no-input
 python manage.py migrate
+
+python manage.py createsuperuser --no-input --username $DJANGO_SUPERUSER_USERNAME --email $DJANGO_SUPERUSER_EMAIL || true
